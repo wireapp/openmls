df6b7f2e6b8918997e8426b843e563e0a75b9c43
Ivan Stanković <ivan.stankovic@wire.com>
2024-07-04 10:45:13 +0200

    chore: remove support for the Kyber-based KEM
    Kyber PQ KEM is still being worked on and has an active draft:
    https://datatracker.ietf.org/doc/html/draft-cfrg-schwabe-kyber-04.
    
    Additionally, the hybrid scheme that combines X25519 and Kyber is
    also in the drafting phase:
    https://www.ietf.org/archive/id/draft-tls-westerbaan-xyber768d00-02.html
    
    Given this, it makes no sense for us to provide support for Kyber-based
    KEMs.

--- openmls_rust_crypto/src/provider.rs
+++ openmls_rust_crypto/src/provider.rs
@@ -96,8 +96,7 @@ impl OpenMlsCrypto for RustCrypto {
             | Ciphersuite::MLS_128_DHKEMX25519_CHACHA20POLY1305_SHA256_Ed25519
             | Ciphersuite::MLS_128_DHKEMP256_AES128GCM_SHA256_P256
             | Ciphersuite::MLS_256_DHKEMP384_AES256GCM_SHA384_P384
-            | Ciphersuite::MLS_256_DHKEMP521_AES256GCM_SHA512_P521
-            | Ciphersuite::MLS_128_X25519KYBER768DRAFT00_AES128GCM_SHA256_Ed25519 => Ok(()),
+            | Ciphersuite::MLS_256_DHKEMP521_AES256GCM_SHA512_P521 => Ok(()),
             _ => Err(CryptoError::UnsupportedCiphersuite),
         }
     }
@@ -109,7 +108,6 @@ impl OpenMlsCrypto for RustCrypto {
             Ciphersuite::MLS_128_DHKEMP256_AES128GCM_SHA256_P256,
             Ciphersuite::MLS_256_DHKEMP384_AES256GCM_SHA384_P384,
             Ciphersuite::MLS_256_DHKEMP521_AES256GCM_SHA512_P521,
-            Ciphersuite::MLS_128_X25519KYBER768DRAFT00_AES128GCM_SHA256_Ed25519,
         ]
     }
 
@@ -483,15 +481,6 @@ impl OpenMlsCrypto for RustCrypto {
                 hpke::kdf::HkdfSha512,
                 hpke::kem::DhP521HkdfSha512,
             >(pk_r, info, aad, ptxt, &mut *rng),
-            HpkeConfig(
-                HpkeKemType::X25519Kyber768Draft00,
-                HpkeKdfType::HkdfSha256,
-                HpkeAeadType::AesGcm128,
-            ) => hpke_core::hpke_seal::<
-                hpke::aead::AesGcm128,
-                hpke::kdf::HkdfSha256,
-                hpke::kem::X25519Kyber768Draft00,
-            >(pk_r, info, aad, ptxt, &mut *rng),
             _ => Err(CryptoError::UnsupportedKem),
         }
     }
@@ -580,21 +569,6 @@ impl OpenMlsCrypto for RustCrypto {
                 aad,
                 input.ciphertext.as_slice(),
             )?,
-            HpkeConfig(
-                HpkeKemType::X25519Kyber768Draft00,
-                HpkeKdfType::HkdfSha256,
-                HpkeAeadType::AesGcm128,
-            ) => hpke_core::hpke_open::<
-                hpke::aead::AesGcm128,
-                hpke::kdf::HkdfSha256,
-                hpke::kem::X25519Kyber768Draft00,
-            >(
-                sk_r,
-                input.kem_output.as_slice(),
-                info,
-                aad,
-                input.ciphertext.as_slice(),
-            )?,
             _ => return Err(CryptoError::UnsupportedKem),
         };
 
@@ -660,15 +634,6 @@ impl OpenMlsCrypto for RustCrypto {
                 hpke::kdf::HkdfSha512,
                 hpke::kem::DhP521HkdfSha512,
             >(pk_r, info, exporter_context, exporter_length, &mut *rng)?,
-            HpkeConfig(
-                HpkeKemType::X25519Kyber768Draft00,
-                HpkeKdfType::HkdfSha256,
-                HpkeAeadType::AesGcm128,
-            ) => hpke_core::hpke_export_tx::<
-                hpke::aead::AesGcm128,
-                hpke::kdf::HkdfSha256,
-                hpke::kem::X25519Kyber768Draft00,
-            >(pk_r, info, exporter_context, exporter_length, &mut *rng)?,
             _ => return Err(CryptoError::UnsupportedKem),
         };
 
@@ -732,15 +697,6 @@ impl OpenMlsCrypto for RustCrypto {
                 hpke::kdf::HkdfSha512,
                 hpke::kem::DhP521HkdfSha512,
             >(enc, sk_r, info, exporter_context, exporter_length)?,
-            HpkeConfig(
-                HpkeKemType::X25519Kyber768Draft00,
-                HpkeKdfType::HkdfSha256,
-                HpkeAeadType::AesGcm128,
-            ) => hpke_core::hpke_export_rx::<
-                hpke::aead::AesGcm128,
-                hpke::kdf::HkdfSha256,
-                hpke::kem::X25519Kyber768Draft00,
-            >(enc, sk_r, info, exporter_context, exporter_length)?,
             _ => return Err(CryptoError::UnsupportedKem),
         };
 
@@ -766,9 +722,6 @@ impl OpenMlsCrypto for RustCrypto {
             }
             HpkeKemType::DhKem25519 => {
                 hpke_core::hpke_derive_keypair::<hpke::kem::X25519HkdfSha256>(ikm)
-            }
-            HpkeKemType::X25519Kyber768Draft00 => {
-                hpke_core::hpke_derive_keypair::<hpke::kem::X25519Kyber768Draft00>(ikm)
             }
             _ => Err(CryptoError::UnsupportedKem),
         }
