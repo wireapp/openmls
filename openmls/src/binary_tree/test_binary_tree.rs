aec92d287ab26fe88968dd9cdaff305541b2db6e
Ivan Stanković <ivan.stankovic@wire.com>
2026-08-14 15:16:05 +0200

    test: remove broken test
    It was dropped in upstream commit 6d3634235c75663ce8c21219ccb8b30a98c87182, and prior to that it was commented out.

--- openmls/src/binary_tree/test_binary_tree.rs
+++ openmls/src/binary_tree/test_binary_tree.rs
@@ -155,24 +155,6 @@ fn test_diff_merging() {
 
 #[test]
 #[wasm_bindgen_test::wasm_bindgen_test]
-fn test_new_tree_error() {
-    // Let's test what happens when the tree is getting too large.
-    let mut nodes: Vec<TreeNode<u32, u32>> = Vec::new();
-
-    // We allow uninitialized vectors because we don't want to allocate so much memory
-    #[allow(clippy::uninit_vec)]
-    unsafe {
-        nodes.set_len(u32::MAX as usize);
-
-        assert_eq!(
-            MlsBinaryTree::new(nodes).expect_err("no error adding beyond TREE_MAX"),
-            MlsBinaryTreeError::OutOfRange
-        )
-    }
-}
-
-#[test]
-#[wasm_bindgen_test::wasm_bindgen_test]
 fn test_diff_iter() {
     let nodes = (0..101)
         .map(|i| {
